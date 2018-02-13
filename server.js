98375330f5e7a2d0dd4686f874271d6f8cd81421
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-02-13 04:34:59 +0300

    installed libs& created models & inserted data

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+
+module.exports = (db, config) => {
+    const app = express();
+
+   
+
+    return app;
+};
