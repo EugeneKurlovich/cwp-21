981b2d47bb60b62ea570cccb46800906d5c9518e
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-02-16 01:58:43 +0300

    added cache & logs

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const {propertySchema, agentSchema, officeSchema} = require('./schemas');
 const PropertyService = require('./services/property');
 const AgentService = require('./services/agent');
 const OfficeService = require('./services/office');
+const LoggerService = require('./services/logger');
+const CacheService = require('./services/cache');
 
 
 module.exports = (db, config) => {
@@ -16,8 +18,12 @@ module.exports = (db, config) => {
    const propertyService = new PropertyService(db.properties, db.agents, propertySchema, errors);
    const agentService = new AgentService(db.agents, db.offices, agentSchema, errors);
    const officeService = new OfficeService(db.offices, officeSchema, errors);
-
+   const loggerService = new LoggerService();
+   const cacheService = new CacheService();
    // Controllers
+   const logger = require('./global-controllers/logger')(loggerService);
+    const error = require('./global-controllers/error');
+    const cache = require('./global-controllers/cache')(cacheService,loggerService);
    const apiController = require('./controllers/api')(
        propertyService,
        agentService,
@@ -27,7 +33,12 @@ module.exports = (db, config) => {
 
    //Mounting
    app.use(bodyParser.json());
+   app.use(express.static('public'));
+   app.use(cookieParser(config.cookie.key));
+   app.use('/api', logger);
+   app.use('/api', cache);
    app.use('/api', apiController);
+   app.use('/api', error);
 
     return app;
 };
